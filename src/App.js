1601b2b853b55801faa34a0c16549e4a32cbc47e
Ana Nikolova <ani.nik19@gmail.com>
2020-10-05 17:55:52 +0200

    pagination done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Posts } from "./components/Posts";
+import Pagination from "./components/Pagination";
 import "./App.css";
 
 function App() {
@@ -18,12 +20,25 @@ function App() {
 
     fetchPosts();
   }, []);
-
   //console.log(posts);
 
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="App">
-      <h1>Hello there</h1>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
